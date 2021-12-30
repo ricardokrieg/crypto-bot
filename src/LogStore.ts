1920e8d7239c2a1fac87d39376aaeef2c6bcd51d
Ricardo Franco <ricardo.krieg@gmail.com>
2021-12-30 12:47:19 -0300

    LogStore store log keyed by block number

--- src/LogStore.ts
+++ src/LogStore.ts
@@ -4,14 +4,28 @@ export interface ILogReceiver {
   onLog: (log: Log) => void
 }
 
-export default class LogStore implements ILogReceiver {
+export interface ILogStore {
+  logs: Log[]
+  blockCount: () => number
+}
+
+export default class LogStore implements ILogReceiver, ILogStore {
   readonly logs: Log[]
+  private readonly blocks: { [blockNumber: number]: string[] }
 
   constructor() {
     this.logs = []
+    this.blocks = {}
   }
 
   onLog(log: Log) {
     this.logs.push(log)
+
+    const block = this.blocks[log.blockNumber] || []
+    this.blocks[log.blockNumber] = [ ...block, log.address ]
+  }
+
+  blockCount(): number {
+    return Object.keys(this.blocks).length
   }
 }
--- tests/unit/LogStore.test.ts
+++ tests/unit/LogStore.test.ts
@@ -3,20 +3,46 @@ import {Log} from 'web3-core'
 import LogStore from '../../src/LogStore'
 
 test('Stores all received logs', async () => {
-  const log: Log = {
-    address: "",
-    blockHash: "",
-    blockNumber: 0,
-    data: "",
-    logIndex: 0,
-    topics: [],
-    transactionHash: "",
-    transactionIndex: 0
+  const logs: Log[] = []
+  const logStore = new LogStore()
+
+  for (let i = 0; i < 2; i++) {
+    const log: Log = {
+      address: "",
+      blockHash: "",
+      blockNumber: 0,
+      data: "",
+      logIndex: 0,
+      topics: [],
+      transactionHash: "",
+      transactionIndex: 0
+    }
+
+    logs.push(log)
+    logStore.onLog(log)
   }
 
-  const logStore = new LogStore()
-  logStore.onLog(log)
+  expect(logStore.logs.length).toBe(2)
+  expect(logStore.logs[0]).toEqual(logs[0])
+})
 
-  expect(logStore.logs.length).toBe(1)
-  expect(logStore.logs[0]).toEqual(log)
+test('Returns how many blocks it has stored', async () => {
+  const logStore = new LogStore()
+
+  for (let i = 0; i < 3; i++) {
+    const log: Log = {
+      address: "",
+      blockHash: "",
+      blockNumber: i === 0 ? 1 : i, // resulting blocks will be: 1, 1, 2
+      data: "",
+      logIndex: 0,
+      topics: [],
+      transactionHash: "",
+      transactionIndex: 0
+    }
+
+    logStore.onLog(log)
+  }
+
+  expect(logStore.blockCount()).toBe(2)
 })
