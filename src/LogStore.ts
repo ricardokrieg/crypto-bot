cafda3532e45234d930e6f041f05796ba4594dfe
Ricardo Franco <ricardo.krieg@gmail.com>
2021-12-30 20:56:02 -0300

    LogStore doesn't need to store all logs

--- src/LogStore.ts
+++ src/LogStore.ts
@@ -5,22 +5,17 @@ export interface ILogReceiver {
 }
 
 export interface ILogStore {
-  logs: Log[]
   blockCount: () => number
 }
 
 export default class LogStore implements ILogReceiver, ILogStore {
-  readonly logs: Log[]
   private readonly blocks: { [blockNumber: number]: string[] }
 
   constructor() {
-    this.logs = []
     this.blocks = {}
   }
 
   onLog(log: Log) {
-    this.logs.push(log)
-
     const block = this.blocks[log.blockNumber] || []
     this.blocks[log.blockNumber] = [ ...block, log.address ]
   }
--- tests/unit/LogStore.test.ts
+++ tests/unit/LogStore.test.ts
@@ -2,30 +2,6 @@ import {Log} from 'web3-core'
 
 import LogStore from '../../src/LogStore'
 
-test('Stores all received logs', async () => {
-  const logs: Log[] = []
-  const logStore = new LogStore()
-
-  for (let i = 0; i < 2; i++) {
-    const log: Log = {
-      address: "",
-      blockHash: "",
-      blockNumber: 0,
-      data: "",
-      logIndex: 0,
-      topics: [],
-      transactionHash: "",
-      transactionIndex: 0
-    }
-
-    logs.push(log)
-    logStore.onLog(log)
-  }
-
-  expect(logStore.logs.length).toBe(2)
-  expect(logStore.logs[0]).toEqual(logs[0])
-})
-
 test('Returns how many blocks it has stored', async () => {
   const logStore = new LogStore()
 
