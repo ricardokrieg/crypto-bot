acdcdb268bdde0cd8798800af93da5849a0186f7
Ricardo Franco <ricardo.krieg@gmail.com>
2021-12-30 21:41:54 -0300

    release detector sends release if log store has 100 or more blocks

--- tests/unit/ReleaseDetector.test.ts
+++ tests/unit/ReleaseDetector.test.ts
@@ -14,13 +14,19 @@ class TestReleaseListener implements IReleaseListener {
 }
 
 class TestLogStore implements ILogStore {
+  private readonly blocks: number
+
+  constructor(blocks: number) {
+    this.blocks = blocks
+  }
+
   blockCount(): number {
-    return 99
+    return this.blocks
   }
 }
 
 describe('When the Log Store has less than 100 blocks', () => {
-  let logStore = new TestLogStore()
+  let logStore = new TestLogStore(99)
 
   beforeEach(() => {
     assert(logStore.blockCount() < 100)
@@ -31,21 +37,48 @@ describe('When the Log Store has less than 100 blocks', () => {
 
     const releaseDetector = new ReleaseDetector(logStore, releaseListener)
 
-    for (let i = 0; i < 3; i++) {
-      const log: Log = {
-        address: "",
-        blockHash: "",
-        blockNumber: 0,
-        data: "",
-        logIndex: 0,
-        topics: [],
-        transactionHash: "",
-        transactionIndex: 0
-      }
-
-      releaseDetector.onLog(log)
+    const log: Log = {
+      address: "",
+      blockHash: "",
+      blockNumber: 0,
+      data: "",
+      logIndex: 0,
+      topics: [],
+      transactionHash: "",
+      transactionIndex: 0
     }
 
+    releaseDetector.onLog(log)
+
     expect(releaseListener.release).toBeUndefined()
+  })
+})
+
+describe('When the Log Store has 100 blocks or more', () => {
+  let logStore = new TestLogStore(100)
+
+  beforeEach(() => {
+    assert(logStore.blockCount() >= 100)
+  })
+
+  test('Emits the Release to the release listener', async () => {
+    const releaseListener = new TestReleaseListener()
+
+    const releaseDetector = new ReleaseDetector(logStore, releaseListener)
+
+    const log: Log = {
+      address: "0x0",
+      blockHash: "",
+      blockNumber: 0,
+      data: "",
+      logIndex: 0,
+      topics: [],
+      transactionHash: "",
+      transactionIndex: 0
+    }
+
+    releaseDetector.onLog(log)
+
+    expect(releaseListener.release).toEqual({ address: "0x0" })
   })
 })
