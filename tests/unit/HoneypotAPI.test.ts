d1bae889a25e8a31e6a9eecfd29ae7fb214178a1
Ricardo Franco <ricardo.krieg@gmail.com>
2022-01-15 13:31:44 -0300

    remove some HoneypotAPI tests

--- tests/unit/HoneypotAPI.test.ts
+++ tests/unit/HoneypotAPI.test.ts
@@ -6,63 +6,15 @@ import {FeeResponse} from '../../src/FeeChecker'
 const contractAddress = '0x1'
 const honeypotAPI = new HoneypotAPI('http://example.com')
 
-test('Returns the correct response when contract has high fees', async () => {
-  axios.get = jest.fn((_url) => {
-    return {
-      data: {
-        BuyGas: 1500000,
-        BuyTax: 90,
-        SellGas: 1800000,
-        SellTax: 80,
-        IsHoneypot: false,
-      }
-    }
-  })
-
-  const response: FeeResponse = await honeypotAPI.check(contractAddress)
-
-  expect(response).toEqual({
-    buyFee: 90,
-    buyGas: 1500000,
-    sellFee: 80,
-    sellGas: 1800000,
-    isHoneypot: false
-  })
-})
-
-test('Returns the correct response when contract has high gas', async () => {
-  axios.get = jest.fn((_url) => {
-    return {
-      data: {
-        BuyGas: 1500000,
-        BuyTax: 10,
-        SellGas: 2100000,
-        SellTax: 10,
-        IsHoneypot: false,
-      }
-    }
-  })
-
-  const response: FeeResponse = await honeypotAPI.check(contractAddress)
-
-  expect(response).toEqual({
-    buyFee: 10,
-    buyGas: 1500000,
-    sellFee: 10,
-    sellGas: 2100000,
-    isHoneypot: false
-  })
-})
-
-test('Returns the correct response when the contract is valid', async () => {
+test('Returns the correct response based on the is.honeypot API response', async () => {
   axios.get = jest.fn((_url) => {
     return {
       data: {
         BuyGas: 1500000,
         BuyTax: 19,
         SellGas: 1800000,
-        SellTax: 18,
-        IsHoneypot: false,
+        SellTax: 17,
+        IsHoneypot: true,
       }
     }
   })
@@ -72,31 +24,7 @@ test('Returns the correct response when the contract is valid', async () => {
   expect(response).toEqual({
     buyFee: 19,
     buyGas: 1500000,
-    sellFee: 18,
-    sellGas: 1800000,
-    isHoneypot: false
-  })
-})
-
-test('Returns the correct response when the contract is a honeypot', async () => {
-  axios.get = jest.fn((_url) => {
-    return {
-      data: {
-        BuyGas: 1500000,
-        BuyTax: 0,
-        SellGas: 1800000,
-        SellTax: 0,
-        IsHoneypot: true,
-      }
-    }
-  })
-
-  const response: FeeResponse = await honeypotAPI.check(contractAddress)
-
-  expect(response).toEqual({
-    buyFee: 0,
-    buyGas: 1500000,
-    sellFee: 0,
+    sellFee: 17,
     sellGas: 1800000,
     isHoneypot: true
   })
